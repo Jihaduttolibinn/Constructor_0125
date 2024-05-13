1780f5cf13eafd9e82648724b401d8ce3671c1df
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-05-13 16:50:27 +0700

    Memasukkan class angka

--- MemberStatic4/MemberStatic4.cpp
+++ MemberStatic4/MemberStatic4.cpp
@@ -1,2 +1,26 @@
 #include <iostream>
+using namespace std;
 
+class angka {
+private:
+    int* arr;
+    int panjang;
+public:
+    angka(int); //constructor
+    ~angka();
+    void cetakData();
+    void isiData();
+};
+
+//Definisi member function
+angka::angka(int i) {//constructor
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+angka::~angka() {//constructor
+    cout << endl;
+    cetakData();
+    delete[]arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
