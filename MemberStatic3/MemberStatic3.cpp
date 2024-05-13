eaf95d37ae5d0ce28034fc08242e177e1c736055
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-05-13 16:47:54 +0700

    memasukkan int main

--- MemberStatic3/MemberStatic3.cpp
+++ MemberStatic3/MemberStatic3.cpp
@@ -10,28 +10,44 @@ public:
 	string nama;
 
 public:
-	Mahasiswa()
-	{
-		nim = 20230140125;
-		nama = "Manchester City";
-
-	};
-	Mahasiswa(int inim) {
-		nim = inim;
-	}
+	Mahasiswa();
+	Mahasiswa(int);
+	Mahasiswa(string);
+	Mahasiswa(int iNim, string iNama);
+	void cetak();
 };
 
-Mahasiswa::Mahasiswa(int iNim)
+Mahasiswa:: Mahasiswa(int iNim)
 {
 	nim = iNim;
 }
-Mahasiswa::Mahasiswa(string iNama)
+Mahasiswa:: Mahasiswa(string iNama)
 {
 	nama = iNama;
 }
-Mahasiswa::Mahasiswa(int iNim, string iNama)
+Mahasiswa:: Mahasiswa(int iNim, string iNama)
 {
 	nim = iNim;
 	nama = iNama;
 }
  
+void Mahasiswa::cetak()
+{
+	cout << endl << "Nim  =" <<nim << endl;
+	cout << " Nama  = " <<nama<< endl;
+
+}
+
+int main() {
+	Mahasiswa mhs1;
+	Mahasiswa mhs2(20);
+	Mahasiswa mhs3("Indra");
+	Mahasiswa mhs4(30, "Fauzan");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
+}
